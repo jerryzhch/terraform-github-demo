2c71fcfda5db8db1b67077a4178fc9da3f3374ec
Jerry <jerry_agboola@hotmail.com>
2023-10-21 15:47:10 +0200

    progress during viscon course

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,17 @@ terraform {
   }
 
   # # Remote backend
-  # backend "azurerm" {
-  #   resource_group_name  = "wga-rg"       # Change to your own resource group name
-  #   storage_account_name = "wgatfstatesa" # Change to your own storage account name
-  #   container_name       = "tfstate"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "wga-rg"       # Change to your own resource group name
+    storage_account_name = "wgatfstatesa" # Change to your own storage account name
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 
   # Uncomment line below in case of provider registration issues
-  # skip_provider_registration = true
+skip_provider_registration = true
 }
